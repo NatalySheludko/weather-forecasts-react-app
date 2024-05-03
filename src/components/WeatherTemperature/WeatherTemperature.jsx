202e3caed4d4ae0f2b3b63ec321788317276f2f8
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-05-04 02:11:02 +0300

    add weatherTemp components

---
+++ src/components/WeatherTemperature/WeatherTemperature.jsx
@@ -0,0 +1,11 @@
+import css from "../WeatherTemperature/WeatherTemperature.module.css";
+
+
+export default function WeatherTemperature(props) {
+  return (
+    <div>
+      <span className={css.currentTemperature}>{Math.round(props.celsius)}</span>
+      <span className={css.unit}>°C</span>
+    </div>
+  );
+}
