ec9a7a3050a15abee9e9b39d90c3714e8594968c
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-05-04 02:11:24 +0300

    add weatherAnimated components

---
+++ src/components/WeatherAnimatedIcon/WeatherAnimatedIcon.jsx
@@ -0,0 +1,32 @@
+import ReactAnimatedWeather from "react-animated-weather";
+
+export default function WeatherAnimatedIcon(props) {
+  const codeMapping = {
+    "clear-sky-day": "CLEAR_DAY",
+    "clear-sky-night": "CLEAR_NIGHT",
+    "few-clouds-day": "PARTLY_CLOUDY_DAY",
+    "few-clouds-night": "PARTLY_CLOUDY_NIGHT",
+    "scattered-clouds-day": "CLOUDY",
+    "scattered-clouds-night": "CLOUDY",
+    "broken-clouds-day": "CLOUDY",
+    "broken-clouds-night": "CLOUDY",
+    "shower-rain-day": "RAIN",
+    "shower-rain-night": "RAIN",
+    "rain-day": "RAIN",
+    "rain-night": "RAIN",
+    "thunderstorm-day": "RAIN",
+    "thunderstorm-night": "RAIN",
+    "snow-day": "SNOW",
+    "snow-night": "SNOW",
+    "mist-day": "FOG",
+    "mist-night": "FOG",
+  };
+  return (
+    <ReactAnimatedWeather
+      icon={codeMapping[props.code]}
+      color="#3758CB"
+      size={props.size}
+      animate={true}
+    />
+  );
+}
