c661adb11a768d5af38f91c95eb28c7b6be9cccb
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-05-05 01:43:36 +0300

    WeatherInfo: add styles

--- src/components/WeatherInfo/WeatherInfo.jsx
+++ src/components/WeatherInfo/WeatherInfo.jsx
@@ -7,45 +7,40 @@ import FormatDate from "../FormatDate/FormatDate";
 export default function WeatherInfo(props) {
   return (
     <div className={css.weatherInfo}>
-      <h2 className={css.heading}>
-        <span className={css.headingText}>Result:</span> {props.data.city}
-      </h2>
-      <div>
-        <div>
-          <div className={css.mainBox}>
-            <div className={css.weatherIcon}>
-              <WeatherAnimatedIcon code={props.data.icon} size={52} />
-            </div>
-            <div>
-              <WeatherTemperature celsius={props.data.temperature} />
-            </div>
+      <div className={css.weather}>
+        <h2 className={css.heading}>{props.data.city}</h2>
+
+        <div className={css.mainBox}>
+          <div>
+            <WeatherAnimatedIcon code={props.data.icon} size={80} />
+          </div>
+          <div>
+            <WeatherTemperature celsius={props.data.temperature} />
           </div>
         </div>
-        <div>
-          <div className={css.feelsLikeBox}>
-            <ul className={css.weatherMenu}>
-              <li>
-                Feels like: {props.data.feels_like}
-                <span className={css.degreeColor}>°</span>
-              </li>
-              <li>Humidity: {props.data.humidity}%</li>
-              <li>Wind: {props.data.wind} km/h</li>
-            </ul>
-          </div>
-        </div>
-        <div>
-          <div className={css.descriptionBox}>
-            <ul className={css.weatherMenu}>
-              <li>
-                <h1 className={css.mainHeading}>Weather</h1>
-              </li>
-              <li>
-                <FormatDate time={props.data.time} />
-              </li>
-              <li className={css.textCapitalize}>{props.data.description}</li>
-            </ul>
-          </div>
-        </div>
+      </div>
+
+      <div className={css.feelsLikeBox}>
+        <ul>
+          <li>
+            Feels like: {props.data.feels_like}
+            <span className={css.degreeColor}>°</span>
+          </li>
+          <li>Humidity: {props.data.humidity}%</li>
+          <li>Wind: {props.data.wind} km/h</li>
+        </ul>
+      </div>
+
+      <div className={css.descriptionBox}>
+        <ul>
+          <li>
+            <h1 className={css.mainTitle}>Weather</h1>
+          </li>
+          <li>
+            <FormatDate time={props.data.time} />
+          </li>
+          <li>{props.data.description}</li>
+        </ul>
       </div>
     </div>
   );
